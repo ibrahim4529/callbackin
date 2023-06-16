f94da1da7f910a7fac427030447b3db758d426fa
ibrahim4529 <ibrahim.hanif4529@gmail.com>
2023-06-16 17:36:03 +0700

    fix: cors for integration web dashboard

--- server/main.py
+++ server/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from routers import auth_router, callback_router
 from utils.db import create_db_and_tables
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost:5173",
+    "https://callbackin.my.id"
+]
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 app.include_router(auth_router)
 app.include_router(callback_router)
 
